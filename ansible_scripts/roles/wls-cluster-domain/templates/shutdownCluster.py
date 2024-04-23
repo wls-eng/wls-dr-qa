9b92f28ac524e1dbf69da91a32a9939c2075c835
gnsuryan <gurudutt.suryanarayana@oracle.com>
2024-04-23 12:51:34 +0000

    deploying replication webapp for verifying access to app deployed on wls cluster

--- ansible_scripts/roles/wls-cluster-domain/templates/shutdownCluster.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-connect('{{ admin_user }}','{{ admin_password }}','{{ admin_t3_url }}')
-state('{{ cluster_name }}','Cluster')
-shutdown('{{ cluster_name }}','Cluster')
-state('{{ cluster_name }}','Cluster')
-disconnect()
-exit()
