1ec1a89cf4bd126523032d7324f2f28a5525312f
gnsuryan <gurudutt.suryanarayana@oracle.com>
2024-07-26 15:27:10 +0000

    Further changes to iptable configuration settings

--- ansible_scripts/roles/setup-global-lb/templates/switch_to_site2.sh
+++ ansible_scripts/roles/setup-global-lb/templates/switch_to_site2.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
 
-#enable port forwarding
-sysctl -w net.ipv4.ip_forward=1
-sysctl -p
-sysctl --system
+result=$(iptables -t nat -L -v -n --line-numbers | grep "${site2_lb_vm_ip}")
+if [ "$result" == "0" ];
+then
+    echo "deleting site2 related iptable rules"
+    iptables -t nat -D PREROUTING -p tcp --dport {{ohs_http_port}} -j DNAT --to-destination {{site2_lb_vm_ip}}:{{ohs_http_port}}
+    iptables -t nat -D PREROUTING -p tcp --dport {{ohs_https_port}} -j DNAT --to-destination {{site2_lb_vm_ip}}:{{ohs_https_port}}
+    iptables -t nat -D PREROUTING -p tcp --dport {{ohs_http_port_for_wls_admin_server}} -j DNAT --to-destination {{site2_lb_vm_ip}}:{{ ohs_http_port_for_wls_admin_server }}
+fi
 
-#Flush all IP Table rules
-iptables -w -F
+result=$(iptables -t nat -L -v -n --line-numbers | grep "${site1_lb_vm_ip}")
+if [ "$result" == "0" ];
+then
+    echo "deleting site1 related iptable rules"
+    iptables -t nat -D PREROUTING -p tcp --dport {{ohs_http_port}} -j DNAT --to-destination {{site1_lb_vm_ip}}:{{ohs_http_port}}
+    iptables -t nat -D PREROUTING -p tcp --dport {{ohs_https_port}} -j DNAT --to-destination {{site1_lb_vm_ip}}:{{ohs_https_port}}
+    iptables -t nat -D PREROUTING -p tcp --dport {{ohs_http_port_for_wls_admin_server}} -j DNAT --to-destination {{site1_lb_vm_ip}}:{{ ohs_http_port_for_wls_admin_server }}
+fi
 
-iptables -w -t nat -A PREROUTING -p tcp --dport {{ohs_http_port}} -j DNAT --to-destination {{site2_lb_vm_ip}}:{{ohs_http_port}}
-iptables -w -t nat -A PREROUTING -p tcp --dport {{ohs_https_port}} -j DNAT --to-destination {{site2_lb_vm_ip}}:{{ohs_https_port}}
-iptables -w -t nat -A PREROUTING -p tcp --dport {{ohs_http_port_for_wls_admin_server}} -j DNAT --to-destination {{site2_lb_vm_ip}}:{{ ohs_http_port_for_wls_admin_server }}
+result=$(iptables -t nat -L -v -n --line-numbers | grep "MASQUERADE")
+if [ "$result" == "0" ];
+then
+    echo "Deleting POSTROUTING MASQUERADE rule"
+    iptables -t nat -D POSTROUTING -j MASQUERADE
+fi
+
+echo "IP Table Rules after Deletion, if any..."
+iptables -t nat -L -v -n --line-numbers
 
 
-iptables -w -t nat -A POSTROUTING -j MASQUERADE
+echo "Adding new IPTable Rules for Routing to Site1..."
+iptables -t nat -A PREROUTING -p tcp --dport {{ohs_http_port}} -j DNAT --to-destination {{site2_lb_vm_ip}}:{{ohs_http_port}}
+iptables -t nat -A PREROUTING -p tcp --dport {{ohs_https_port}} -j DNAT --to-destination {{site2_lb_vm_ip}}:{{ohs_https_port}}
+iptables -t nat -A PREROUTING -p tcp --dport {{ohs_http_port_for_wls_admin_server}} -j DNAT --to-destination {{site2_lb_vm_ip}}:{{ ohs_http_port_for_wls_admin_server }}
+iptables -t nat -A POSTROUTING -j MASQUERADE
 
-#view port forwarding rules
-iptables -t nat -L
-
+echo "IP Table Rules after Adding new rules to Site1"
+iptables -t nat -L -v -n --line-numbers
