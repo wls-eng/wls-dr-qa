8c6ad70d168708d0cc0f1c7ce47bc70beee5c87d
gnsuryan <gurudutt.suryanarayana@oracle.com>
2024-07-30 12:32:21 +0000

    deploy session counter app and provide sudo privilege to oracle user

--- ansible_scripts/roles/wls-dynamic-cluster-domain/templates/deployApp.py
+++ ansible_scripts/roles/wls-dynamic-cluster-domain/templates/deployApp.py
@@ -2,7 +2,9 @@ import os
 import sys
 
 connect('{{ admin_user }}','{{ admin_password }}','{{ admin_t3_url }}')
-deploy('{{deployAppName}}','{{ work_dir }}/{{deployAppFileName}}',targets='{{cluster_name}}',remote='true', upload='true')
-startApplication('{{deployAppName}}')
+deploy('{{replicationWebAppName}}','{{ work_dir }}/{{replicationWebAppFileName}}',targets='{{cluster_name}}',remote='true', upload='true')
+deploy('{{sessionCounterAppName}}','{{ work_dir }}/{{sessionCounterAppFileName}}',targets='{{cluster_name}}',remote='true', upload='true')
+startApplication('{{replicationWebAppName}}')
+startApplication('{{sessionCounterAppName}}')
 disconnect()
 exit()
